619fe84aa458be90fc50bf6e86ba786343ac1b6f
shixiaowen03 <shixiaowen03@meituan.com>
2018-11-04 21:00:19 +0800

    DIEN

--- recommendation/Basic-DIEN-Demo/train.py
+++ recommendation/Basic-DIEN-Demo/train.py
@@ -79,6 +79,34 @@ def prepare_data(input,target,maxlen = None,return_neg=False):
 
 
 
+def eval(sess, test_data, model, model_path):
+
+    loss_sum = 0.
+    accuracy_sum = 0.
+    aux_loss_sum = 0.
+    nums = 0
+    stored_arr = []
+    for src, tgt in test_data:
+        nums += 1
+        uids, mids, cats, mid_his, cat_his, mid_mask, target, sl, noclk_mids, noclk_cats = prepare_data(src, tgt, return_neg=True)
+        prob, loss, acc, aux_loss = model.calculate(sess, [uids, mids, cats, mid_his, cat_his, mid_mask, target, sl, noclk_mids, noclk_cats])
+        loss_sum += loss
+        aux_loss_sum = aux_loss
+        accuracy_sum += acc
+        prob_1 = prob[:, 0].tolist()
+        target_1 = target[:, 0].tolist()
+        for p ,t in zip(prob_1, target_1):
+            stored_arr.append([p, t])
+    test_auc = calc_auc(stored_arr)
+    accuracy_sum = accuracy_sum / nums
+    loss_sum = loss_sum / nums
+    aux_loss_sum / nums
+    global best_auc
+    if best_auc < test_auc:
+        best_auc = test_auc
+        model.save(sess, model_path)
+    return test_auc, loss_sum, accuracy_sum, aux_loss_sum
+
 def train(train_file = "data/local_train_splitByUser",
         test_file = "data/local_test_splitByUser",
         uid_voc = "data/uid_voc.pkl",
@@ -128,6 +156,29 @@ def train(train_file = "data/local_train_splitByUser",
                     print('save model iter: %d' % (iter))
                     model.save(sess, model_path + "--" + str(iter))
             lr *= 0.5
+
+
+def test(
+        train_file = "local_train_splitByUser",
+        test_file = "local_test_splitByUser",
+        uid_voc = "uid_voc.pkl",
+        mid_voc = "mid_voc.pkl",
+        cat_voc = "cat_voc.pkl",
+        batch_size = 128,
+        maxlen = 100,
+        model_type = 'DNN',
+	seed = 2
+):
+
+    model_path = "dnn_best_model/ckpt_noshuff" + model_type + str(seed)
+    gpu_options = tf.GPUOptions(allow_growth=True)
+    with tf.Session(config=tf.ConfigProto(gpu_options=gpu_options)) as sess:
+        train_data = DataIterator(train_file, uid_voc, mid_voc, cat_voc, batch_size, maxlen)
+        test_data = DataIterator(test_file, uid_voc, mid_voc, cat_voc, batch_size, maxlen)
+        n_uid, n_mid, n_cat = train_data.get_n()
+        model = Model_DIN_V2_Gru_Vec_attGru_Neg(n_uid, n_mid, n_cat, EMBEDDING_DIM, HIDDEN_SIZE, ATTENTION_SIZE)
+        model.restore(sess, model_path)
+        print('test_auc: %.4f ----test_loss: %.4f ---- test_accuracy: %.4f ---- test_aux_loss: %.4f' % eval(sess, test_data, model, model_path))
 
 
 if __name__ == '__main__':
