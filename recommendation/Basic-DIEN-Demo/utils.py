41bff2794365631f0167a99b849645d0cec2156d
shixiaowen03 <shixiaowen03@meituan.com>
2018-11-04 19:15:20 +0800

    dien

--- recommendation/Basic-DIEN-Demo/utils.py
+++ recommendation/Basic-DIEN-Demo/utils.py
@@ -7,6 +7,27 @@ def prelu(_x, scope=''):
                                  dtype=_x.dtype, initializer=tf.constant_initializer(0.1))
         return tf.maximum(0.0, _x) + _alpha * tf.minimum(0.0, _x)
 
+def dice(_x,axis=-1,epsilon=0.000000001,name=""):
+    with tf.variable_scope(name,reuse=tf.AUTO_REUSE):
+        alphas = tf.get_variable('alpha'+name,_x.get_shape()[-1],initializer=tf.constant_initializer(0.0),dtype=tf.float32)
+        input_shape = list(_x.get_shape())
+
+        reduction_axis = list(range(len(input_shape)))
+        del reduction_axis[axis]
+        broadcast_shape = [1] * len(input_shape)
+        broadcast_shape[axis] = input_shape[axis]
+
+    mean = tf.reduce_mean(_x, axis=reduction_axis)
+    brodcast_mean = tf.reshape(mean, broadcast_shape)
+    std = tf.reduce_mean(tf.square(_x - brodcast_mean) + epsilon, axis=reduction_axis)
+    std = tf.sqrt(std)
+    brodcast_std = tf.reshape(std, broadcast_shape)
+    x_normed = (_x - brodcast_mean) / (brodcast_std + epsilon)
+    # x_normed = tf.layers.batch_normalization(_x, center=False, scale=False)
+    x_p = tf.sigmoid(x_normed)
+
+    return alphas * (1.0 - x_p) * _x + x_p * _x
+
 
 def din_fcn_attention(query, facts, attention_size, mask, stag='null', mode='SUM', softmax_stag=1, time_major=False, return_alphas=False, forCnn=False):
     if isinstance(facts, tuple):
