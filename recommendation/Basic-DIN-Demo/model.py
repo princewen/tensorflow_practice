25df4919f94561611698e24e4fdc2461dc3b00fb
bincai(蔡斌) <bincai@tencent.com>
2018-11-15 20:12:18 +0800

    remove .idea

--- recommendation/Basic-DIN-Demo/model.py
+++ recommendation/Basic-DIN-Demo/model.py
@@ -2,10 +2,8 @@ import tensorflow as tf
 
 from Dice import dice
 
-
 class Model(object):
     def __init__(self,user_count,item_count,cate_count,cate_list):
-
         # self.u = tf.placeholder(tf.int32,[None,],name='user')
         self.i = tf.placeholder(tf.int32,[None,],name='item')
         self.j = tf.placeholder(tf.int32,[None,],name='item_j')
@@ -48,7 +46,7 @@ class Model(object):
             tf.nn.embedding_lookup(cate_emb_w, self.hc),
         ], axis=2)
 
-        hist = attention(i_emb,h_emb,self.sl)
+        hist = attention(i_emb, h_emb, self.sl)
 
         hist = tf.layers.batch_normalization(inputs=hist)
         hist = tf.reshape(hist,[-1,hidden_units])
@@ -174,7 +172,7 @@ def extract_axis_1(data, ind):
     return res
 
 
-def attention(queries,keys,keys_length):
+def attention(queries, keys, keys_length):
     '''
         queries:     [B, H]
         keys:        [B, T, H]
@@ -185,7 +183,7 @@ def attention(queries,keys,keys_length):
     queries = tf.tile(queries,[1,tf.shape(keys)[1]])
     queries = tf.reshape(queries,[-1,tf.shape(keys)[1],queries_hidden_units])
 
-    din_all = tf.concat([queries,keys,queries-keys,queries * keys],axis=-1) # B*T*4H
+    din_all = tf.concat([queries, keys, queries-keys, queries * keys], axis=-1) # B*T*4H
     # 三层全链接
     d_layer_1_all = tf.layers.dense(din_all, 80, activation=tf.nn.sigmoid, name='f1_att')
     d_layer_2_all = tf.layers.dense(d_layer_1_all, 40, activation=tf.nn.sigmoid, name='f2_att')
@@ -197,7 +195,7 @@ def attention(queries,keys,keys_length):
     key_masks = tf.sequence_mask(keys_length,tf.shape(keys)[1])
     key_masks = tf.expand_dims(key_masks,1) # B*1*T
     paddings = tf.ones_like(outputs) * (-2 ** 32 + 1) # 在补足的地方附上一个很小的值，而不是0
-    outputs = tf.where(key_masks,outputs,paddings) # B * 1 * T
+    outputs = tf.where(key_masks, outputs, paddings) # B * 1 * T
 
 
     # Scale
